f590841c8acd5605990db1a7b3d14e63b96398ae
Rishi-314 <rishikesh2048x@gmail.com>
2026-07-07 12:16:57 +0530

    Create solution2.java

---
+++ Arrays/isDuplicate/solution2.java
@@ -0,0 +1,18 @@
+package Arrays.isDuplicate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class solution2 {
+    public boolean isDuplicate(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
